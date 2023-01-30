1edb86090cfc868b239d9a53a6e9b7ddb490451b
Antonio Iturriaga <91393576+AntonioCait@users.noreply.github.com>
2023-01-30 17:57:55 -0600

    animated presence mode wait

--- src/main.tsx
+++ src/main.tsx
@@ -16,7 +16,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<Navbar />
-			<AnimatePresence exitBeforeEnter>
+			<AnimatePresence mode="wait">
 				<Routes>
 					<Route path="/" element={<App />} />
 					<Route path="/about" element={<About />} />
